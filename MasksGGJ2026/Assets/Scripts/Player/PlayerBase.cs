029b2e74c1a7e71aa9e0eb5a113e976b0946e512
GuiFritis <guilhermecfritis@gmail.com>
2026-01-31 21:34:56 -0300

    máscara de fantasma

--- MasksGGJ2026/Assets/Scripts/Player/PlayerBase.cs
+++ MasksGGJ2026/Assets/Scripts/Player/PlayerBase.cs
@@ -8,18 +8,6 @@ public class PlayerBase : MonoBehaviour
 
     public event Action OnDeath;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
